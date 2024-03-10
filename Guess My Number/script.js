6b8ec3b8b10c5a0e2bb899c27bdbbdc48b9f0c43
Qheuss <36819862+Qheuss@users.noreply.github.com>
2024-03-10 18:18:26 +0100

    Add files via upload

---
+++ Guess My Number/script.js
@@ -0,0 +1,50 @@
+'use strict';
+
+let number = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+const displayScore = function (score) {
+  document.querySelector('.score').textContent = score;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+  if (!guess) {
+    displayMessage('😒 No number!');
+  } else if (guess === number) {
+    displayMessage('🥳 Correct Number!');
+    document.querySelector('.number').textContent = number;
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== number) {
+    if (score > 1) {
+      displayMessage(guess > number ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      displayScore(score);
+    } else {
+      displayMessage('👿 You lost!');
+      displayScore(0);
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  number = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  displayScore(score);
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+});
